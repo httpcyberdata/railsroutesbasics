985f24a96b3cdfc2d29ff899100062853d9da79c
httpcyberdata <borgeringstad@Brge-Ringstad-PC.local>
2022-10-05 18:35:46 -0400

    Replaced our custom index and show to resources: and only:

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: 'products#index'
 
-  get 'super_cheap_products', to: 'products#index'
-  get 'products', to: 'products#index', as: 'products'
-  get 'products/:id', to: 'products#show', as: 'product', id: /\d+/
+  resources :products, only: [:index, :show]
+
+#   get 'products', to: 'products#index', as: 'products'
+#   get 'products/:id', to: 'products#show', as: 'product', id: /\d+/
 end
